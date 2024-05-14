943824a85622ce1f0fbe75f8576b3dbc184feb17
ChatchananM <165884301+ChatchananM@users.noreply.github.com>
2024-05-15 00:27:05 +0700

    First commit

---
+++ ifElseCondition.js
@@ -0,0 +1,17 @@
+let dayNumber = 7;
+
+if ((dayNumber == 0)) {
+  console.log(dayNumber, "= Sunday");
+} else if ((dayNumber == 1)) {
+  console.log(dayNumber, "= Monday");
+} else if ((dayNumber == 2)) {
+  console.log(dayNumber, "= Tuesday");
+} else if ((dayNumber == 3)) {
+  console.log(dayNumber, "= Wednesday");
+} else if ((dayNumber == 4)) {
+  console.log(dayNumber, " = Thursday");
+} else if ((dayNumber == 5)) {
+  console.log(dayNumber, " = Friday");
+} else if ((dayNumber == 6)) {
+  console.log(dayNumber, " = Saturday");
+} else console.log(dayNumber, " = Invalid day number");
